30c7707e05cd7efd4cc20caf04df443f026abab6
unknown <Hiromi.Kaede@gmail.com>
2015-11-15 22:40:32 +0800

    update

--- bin/run.bat
+++ bin/run.bat
@@ -1,8 +1,10 @@
 @echo off
+cd/d "%~dp0"
 
 set "PYTHON_HOME=%~dp0..\Python35\amd64\Lib\site-packages\lldb"
 del "%PYTHON_HOME%\_lldb.pyd"
 del "%PYTHON_HOME%\lldb-argdumper.exe"
 mklink /H "%PYTHON_HOME%\_lldb.pyd" "%~dp0liblldb.dll"
 mklink /H "%PYTHON_HOME%\lldb-argdumper.exe" "%~dp0lldb-argdumper.exe"
-pause
+
+start lldb.exe
--- bin32/run.bat
+++ bin32/run.bat
@@ -1,8 +1,12 @@
 @echo off
+cd/d "%~dp0"
 
 set "PYTHON_HOME=%~dp0..\Python35\x86\Lib\site-packages\lldb"
 del "%PYTHON_HOME%\_lldb.pyd"
 del "%PYTHON_HOME%\lldb-argdumper.exe"
+del .lldbinit
 mklink /H "%PYTHON_HOME%\_lldb.pyd" "%~dp0liblldb.dll"
 mklink /H "%PYTHON_HOME%\lldb-argdumper.exe" "%~dp0lldb-argdumper.exe"
-pause
+mklink /H .lldbinit "..\bin\.lldbinit"
+
+start lldb.exe
