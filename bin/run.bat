767103a5ac76fa8568f14daed57b4d376ee00676
Ouroboros <Hiromi.Kaede@gmail.com>
2017-05-03 00:53:31 +0800

    upgrade python36.dll to 3.6.1

--- bin/run.bat
+++ bin/run.bat
@@ -4,6 +4,8 @@ cd/d "%~dp0"
 set "PYTHON_HOME=%~dp0..\Python36\amd64\Lib\site-packages\lldb"
 del "%PYTHON_HOME%\_lldb.pyd" 2>NUL
 del "%PYTHON_HOME%\lldb-argdumper.exe" 2>NUL
+del "%~dp0python36.dll"
+mklink /H "%~dp0python36.dll" "%PYTHON_HOME%\..\..\..\python36.dll"
 mklink /H "%PYTHON_HOME%\_lldb.pyd" "%~dp0liblldb.dll"
 mklink /H "%PYTHON_HOME%\lldb-argdumper.exe" "%~dp0lldb-argdumper.exe"
 
