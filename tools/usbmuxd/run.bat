078f0c3dfe853828c8951a393db257ddb50dd133
Arianrhod <hiromi.kaede@gmail.com>
2024-01-26 13:11:25 +0800

    sync

--- tools/usbmuxd/run.bat
+++ tools/usbmuxd/run.bat
@@ -1,5 +1,2 @@
 @echo off
-
-start /B "" "%~dp0tcprelay.py" -t 22:2222
-start /B "" "%~dp0tcprelay.py" -t 5000:5000
-call "%~dp0tcprelay.py" -t 8888:4321
+call "%~dp0tcprelay.py" -t 8888:4321 22:2222 39001:39001
